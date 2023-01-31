94d07ca053eb0d9b49be2ea1225391ffadb0872a
ochieng-kamau <ochiengkamauj@gmail.com>
2023-01-31 18:23:45 +0300

    Added top 10 hot posts script and test file

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -6,13 +6,15 @@ import requests
 
 
 def top_ten(subreddit):
-    """Return the top 10 hot posts on a given subreddit."""
+    """queries the Reddit API and prints the titles of the
+    first 10 hot posts listed for a given subreddit"""
+
     headers = {'User-Agent': 'MyRedditBot/0.0.1'}
     url = f"https://www.reddit.com/r/{subreddit}/hot.json?limit=10"
     response = requests.get(url, headers=headers, allow_redirects=False)
     if response.status_code == 200:
-        data = response.json()['data']['children']
-        for post in data:
+        posts = response.json()['data']['children']
+        for post in posts:
             # subscribers = data['data']['subscribers']
             print(post['data']['title'])
     else:
