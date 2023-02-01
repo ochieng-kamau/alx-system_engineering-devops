846c2bf4e0960db043587e0e0344be3508e9f0f3
ochieng-kamau <ochiengkamauj@gmail.com>
2023-02-01 04:11:11 +0300

    Add file

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,34 +1,22 @@
 #!/usr/bin/python3
-"""
-Requests module for sending HTTP requests to the Reddit API
-"""
+"""Function to print hot posts on a given Reddit subreddit."""
 import requests
 
 
 def top_ten(subreddit):
-    """queries the Reddit API and prints the titles of the
-    first 10 hot posts listed for a given subreddit"""
-    u_agent = 'Mozilla/5.0'
-
+    """Print the titles of the 10 hottest posts on a given subreddit."""
+    url = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
     headers = {
-        'User-Agent': 'u_agent'
+        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
     }
-
     params = {
-        'limit' = 10
+        "limit": 10
     }
-
-    url = f"https://www.reddit.com/r/{subreddit}/hot.json?limit"
-    response = requests.get(
-        url,
-        headers=headers,
-        params=params,
-        allow_redirects=False)
-    if (response.status_code != 200):
-        print(None)
+    response = requests.get(url, headers=headers, params=params,
+                            allow_redirects=False)
+    if response.status_code == 404:
+        print("None")
         return
-    posts = request.json()['data']['children']
-    if len(posts) is 0:
-        print(None)
-    else:
-        print(post['data']['title'])
+    results = response.json().get("data")
+    [print(c.get("data").get("title")) for c in results.get("children")]
+
